68e223ebb0ab94d1614681767ae8bb84ab4de96a
Ian Cottam <ian.cottam@bbc.co.uk>
2018-12-17 19:09:21 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,15 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+     
+    // This is the function that would be invoked by the event listener.
+    function onKeyDownHandler(e) {
+      console.log(e);
+      const key = parseInt(e.detail || e.which);
+      console.log(key);
+    }
+    
+    document.body.addEventListener('keydown', onKeyDownHandler)
 }
+
+init();
