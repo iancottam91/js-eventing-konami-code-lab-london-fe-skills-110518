e1a05c14fd5834e429f3d76ca4767c663c08e141
Ian Cottam <ian.cottam@bbc.co.uk>
2018-12-17 19:36:37 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,9 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-    // This is the function that would be invoked by the event listener.
-    function onKeyDownHandler(e) {
-      console.log(e);
-      const key = parseInt(e.detail || e.which);
-      console.log(key);
-    }
-    
-    document.body.addEventListener('keydown', onKeyDownHandler)
+
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+  console.log('hi');
+}
+
+document.body.addEventListener('keydown', onKeyDownHandler)
 =
