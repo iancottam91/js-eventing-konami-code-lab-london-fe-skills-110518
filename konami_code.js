e0abdae4c3bb6644e664892eadb50ec11b862a34
Ian Cottam <ian.cottam@bbc.co.uk>
2018-12-17 19:39:53 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,13 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0;
+
 // This is the function that would be invoked by the event listener.
 function onKeyDownHandler(event) {
   console.log(event.keyCode);
-  if(event.keyCode === code[0]) {
+  if(event.keyCode === code[index]) {
     console.log('pressed up');
+    index++;
   } else {
     console.log('didnt press upp');
   }
