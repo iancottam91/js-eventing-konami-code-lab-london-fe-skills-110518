59d6c5674dd29e441abfbfd5a03eabf85a9a5206
Ian Cottam <ian.cottam@bbc.co.uk>
2018-12-17 19:37:24 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,8 @@
 
 
 // This is the function that would be invoked by the event listener.
-function onKeyDownHandler(e) {
-  console.log(e);
+function onKeyDownHandler(event) {
+  console.log(event);
 }
 
 document.body.addEventListener('keydown', onKeyDownHandler)
